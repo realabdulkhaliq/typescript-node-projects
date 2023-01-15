768b62a21600bc2d6d2b5fc5494dcd138270bbd7
NewAgeDeveloper <thenewagedevelopers@gmail.com>
2023-01-15 21:26:08 +0500

    CLI Calculator

---
+++ CLI_Calculator/index.ts
@@ -0,0 +1,39 @@
+/*
+Develop a simple command line calculator using TypeScipt, Node.js and Inquirer.
+*/
+
+import inquirer from "inquirer";
+
+(
+    async function calculation(){
+        let input:{firstNumber:number, secondNumber:number, operation:string} = await inquirer.prompt([{
+            "name": "operation",
+            "type": "list",
+            "message":"What You Want to Do",
+            choices: ()=>{
+                return ["Add","Subtract","Multiply","Divide"]
+            }
+        },{
+            "name": "firstNumber",
+            "type": "input",
+            "message": "Please Enter First Number"
+        },{
+            "name": "secondNumber",
+            "type": "input",
+            "message": "Please Enter Second Number"
+        }
+    ])
+        if(input.operation === "Add"){
+            console.log(input.firstNumber + input.secondNumber);
+        }
+        if(input.operation==="Subtract"){
+            console.log(input.firstNumber - input.secondNumber);
+        }
+        if(input.operation==="Multiply"){
+            console.log(input.firstNumber * input.secondNumber);
+        }
+        if(input.operation==="Divide"){
+            console.log(input.firstNumber / input.secondNumber);
+        }
+    }
+)()
